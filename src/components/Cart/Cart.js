d861f742b33c5653a39fe3f8ac1a51c747587f71
mdatikHasan <atikh2030@gmail.com>
2022-08-24 20:21:39 +0600

    cart debug and set

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
     console.log(cart);
-    return (
-        <div>
-            <h3>Order Summary</h3>
-            <p>Selected Item: {cart.length}</p>
 
-            {/* <p>Total Price: ${sum}</p>
-            <p>Shipping Charge: ${shippingCharge}</p>
-            <p>Tax: ${tax}</p>
-            <h3>Grand Total: ${grandTotal}</h3>
-            <button onClick={() => clearCart(cart)}>Clear Cart</button> */}
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(3))
+    const grandTotal = total+ shipping + tax
+    return (
+        <div className='cart'>
+            <h3>Order Summary</h3>
+            <p>Selected Item: {quantity}</p>
+
+            <p>Total Price: ${total}</p>
+            <p>Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h3>Grand Total: ${(grandTotal.toFixed(2))}</h3>
+            {/* <button onClick={() => clearCart(cart)}>Clear Cart</button> */}
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -7,20 +8,6 @@ const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
 
-    let sum = 0;
-    for (const product of cart) {
-        sum = sum + product.price;
-    }
-
-    const shippingCharge = (sum * 1) / 100;
-    const tax = (sum * 5) / 100;
-
-    const grandTotal = sum + shippingCharge + tax;
-
-    const clearCart = cart => {
-        setCart([])
-    }
-
 
     useEffect(() => {
         fetch('products.json')
@@ -28,9 +15,49 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
-    const handleAddToCart = (product) => {
-        const newCart = [...cart, product]
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const saveCart = []
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct)
+            }
+
+        }
+        setCart(saveCart)
+    }, [products])
+
+    /*  useEffect(()=>{
+         const storedCart = getStoredCart();
+         const savedCart = []
+         for(const id in storedCart){
+             const addedProduct =  products.find(product => product.id == id);
+             if(addedProduct){
+                 const quantity = storedCart[id];
+                 addedProduct.quantity = quantity;
+                 savedCart.push(addedProduct)
+             }
+         }
+         setCart(savedCart)
+     },[products]) */
+
+    const handleAddToCart = (selectedProduct) => {
+        let newCart = []
+        const exists = cart.find(product => product.id === selectedProduct.id)
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct]
+        }else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id)
+            exists.quantity  = exists.quantity + 1;
+            newCart = [...rest, exists]
+        }
+        
         setCart(newCart)
+        addToDb(selectedProduct.id)
         // console.log(newCart);
     }
     return (
